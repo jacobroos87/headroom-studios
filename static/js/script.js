828931aadf32c08ec26bb96b366104d3ac0d37cb
jacobroos87 <jacobroos87@gmail.com>
2021-04-19 21:41:10 +0000

    Added quotes over hero img with JS logic

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,14 @@
 $(document).ready(function () {
     $('.sidenav').sidenav({ edge: "right" });
     $(".dropdown-trigger").dropdown();
+    $('.quote-list > li:gt(0)').hide();
+    setInterval(function() {
+        $('.quote-list > li:first')
+        .fadeOut(1500)
+        .next()
+        .delay(1500)
+        .fadeIn(1500)
+        .end()
+        .appendTo('.quote-list');
+        },  7000);
 });

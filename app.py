ac6be383f2c454d6c0a9c80e9d5700748992bb7f
jacobroos87 <jacobroos87@gmail.com>
2021-04-21 16:44:34 +0000

    Added logic for logout and updated navbar

--- app.py
+++ app.py
@@ -76,6 +76,14 @@ def login():
     return render_template("login.html")
 
 
+@app.route("/logout")
+def logout():
+    # remove user from session cookies
+    flash("You have been logged out")
+    session.pop("user")
+    return redirect(url_for("login"))
+
+
 @app.route("/bookings")
 def bookings():
     return render_template("bookings.html")
